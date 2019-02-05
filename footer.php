554b7b6f7161dd0513604b9492888b6e117127ad
Manny <coachmannytw@gmail.com>
2019-02-04 20:33:57 -0700

    updated footer

--- footer.php
+++ footer.php
@@ -56,7 +56,6 @@
               <li><a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
               <li><a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
               <li><a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-              <li><a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
             </ul>
           </nav>
         </div>
